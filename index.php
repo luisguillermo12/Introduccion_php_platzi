5464e9fb4eaf03f2ab87054a4d37c53a443a6704
luisguillermo12 <luisguillermo12@gmail.com>
2019-03-06 22:54:17 -0300

    primeras 3 clases php 7.2.8 anotaciones

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+/* * php es un leguaje interpretado
+
+   * multiplataformas win , linux 
+
+   * es open source , el codigo fuente esta disponible 
+   * tiene muchas integraciones , con diferentes bases de datos y protocolos.
+   * no es un lenguaje compilado , 
+   * no es para aplicaciones de escritorio 
+   * debemos buscar la mejor herramienta dependiendo del tipo de trabajo a desarrollar php =  web 
+
+   xampp = servidor http , php integrado ,  	base de datos (solo desarrollo)
+
+   X= cualquier sistema operativo
+   A=apache
+   M=maria db , derivado de mysql q funciona totalmente igual
+   P=php
+   P=per
+   
+
+ */
+
+?>
